411998f054a6817802b0e3807a961177ee45c118
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-06-26 13:19:19 -0600

    added functionality to dismiss message

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ button.addEventListener("click", submitEmail);
 
 function submitEmail() {
     var thanks = document.getElementById('thank-you-message');
-    thanks.style.display = "block";
     var main = document.getElementById('cover-info');
     main.style.display = "none";
+    thanks.style.display = "block";
+}
+
+const dismissButton = document.getElementById('dismiss');
+
+dismissButton.addEventListener("click", dismissMessage)
+
+function dismissMessage() {
+    var thanks = document.getElementById('thank-you-message');
+    var main = document.getElementById('cover-info');
+    main.style.display = "block";
+    thanks.style.display = "none";
 }
