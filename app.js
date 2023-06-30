27f1d1c15d8616851fcf95d21ed55ebb049b3167
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-06-30 13:07:54 -0600

    added the start of email validation
    -more desktop styling
    -styled thank you page for desktop
    -changed dismiss button function

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-const button = document.getElementById('send-button')
+const button = document.getElementById("send-button");
 
 button.addEventListener("click", submitEmail);
 
 function submitEmail() {
-    var thanks = document.getElementById('thank-you-message');
-    var main = document.getElementById('cover-info');
-    main.style.display = "none";
-    thanks.style.display = "block";
+	var thanks = document.getElementById("thank-you-message");
+	var main = document.getElementById("cover-info");
+	main.style.display = "none";
+	thanks.style.display = "block";
 }
 
-const dismissButton = document.getElementById('dismiss');
+const dismissButton = document.getElementById("dismiss");
 
-dismissButton.addEventListener("click", dismissMessage)
+dismissButton.addEventListener("click", dismissMessage);
 
 function dismissMessage() {
-    var thanks = document.getElementById('thank-you-message');
-    var main = document.getElementById('cover-info');
-    main.style.display = "block";
-    thanks.style.display = "none";
+	location.reload();
+}
+
+var input = document.getElementById("email");
+console.log(input);
+
+function ValidateEmail(input) {
+	var validRegex =
+		/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+	if (input.value.match(validRegex)) {
+		alert("Valid email address!");
+
+		document.input.focus();
+
+		return true;
+	} else {
+		alert("Invalid email address!");
+
+		document.form1.text1.focus();
+
+		return false;
+	}
 }
