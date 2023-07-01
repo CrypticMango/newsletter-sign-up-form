4d23f35888d711f6341ba38b50719eb7735042bf
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-07-01 13:50:21 -0600

    added email validation
    -styling for buttons

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function submitEmail() {
 	var main = document.getElementById("cover-info");
 	main.style.display = "none";
 	thanks.style.display = "block";
+	ValidateEmail();
 }
 
 const dismissButton = document.getElementById("dismiss-button");
@@ -16,27 +17,3 @@ dismissButton.addEventListener("click", dismissMessage);
 function dismissMessage() {
 	location.reload();
 }
-
-var input = document.getElementById("email");
-console.log(input);
-
-function ValidateEmail(input) {
-	var validRegex =
-		/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-	if (input.value.match(validRegex)) {
-		alert("Valid email address!");
-
-		document.input.focus();
-
-		return true;
-	} else {
-		alert("Invalid email address!");
-
-		document.input.focus();
-
-		return false;
-	}
-}
-
-ValidateEmail();
